819d491756b61a89aa99be8e337f28e036ae86a3
Bryan Yoo <bryanyoo5433@gmail.com>
2022-01-15 01:32:02 -0800

    Finished Unit1 Assignments
    - Completed StarsAndStripes Assignment.
    - Completed the extra-credit assignment. (MyName.java file and NameRunner.java file)

---
+++ Unit 01/src/MyName.java
@@ -0,0 +1,55 @@
+//extra credit assignment
+public class MyName{ 
+	public MyName(){
+		System.out.print("My Name");
+		printTwoBlankLines();
+		
+	}
+	public void printTwoBlankLines()
+	{
+		System.out.println();
+		System.out.println();
+	}
+	public void letterB(){
+		System.out.println("___");
+		System.out.println("|  \\");
+		System.out.println("|  |");
+		System.out.println("|__/");
+		System.out.println("|  \\");
+		System.out.println("|   |");
+		System.out.println("|___/");
+	}
+	public void letterR() {
+		System.out.println("___");
+		System.out.println("|  \\");
+		System.out.println("|  |");
+		System.out.println("|__/");
+		System.out.println("| \\");
+		System.out.println("|  \\");
+		System.out.println("|   \\");
+	}
+	public void letterY() {
+		System.out.println("\\    /");
+		System.out.println(" \\  /");
+		System.out.println("  \\/");
+		System.out.println("  |");
+		System.out.println("  |");
+		System.out.println("  |");
+	}
+	public void letterA() {
+		System.out.println("  /\\");
+		System.out.println(" /  \\");
+		System.out.println("/____\\");
+		System.out.println("|    |");
+		System.out.println("|    |");
+		System.out.println("|    |");
+	}
+	public void letterN() {
+		System.out.println("|\\     |");
+		System.out.println("| \\    |");
+		System.out.println("|  \\   |");
+		System.out.println("|   \\  |");
+		System.out.println("|    \\ |");
+		System.out.println("|     \\|");
+	}
+}
---
+++ Unit 01/src/NameRunner.java
@@ -0,0 +1,15 @@
+public class NameRunner {
+	public static void main(String[] args){
+		MyName n = new MyName();
+		n.letterB();
+		n.printTwoBlankLines();
+		n.letterR();
+		n.printTwoBlankLines();
+		n.letterY();
+		n.printTwoBlankLines();
+		n.letterA();
+		n.printTwoBlankLines();
+		n.letterN();
+		n.printTwoBlankLines();
+	}
+}
